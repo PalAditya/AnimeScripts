6b0d7bfaa0c58da591a754696c06c6a8155bccce
PalAditya <adityapal.nghss@gmail.com>
2020-01-27 21:39:53 +0530

    Migrate to actual anime on click

--- WaifuBackground/content.js
+++ WaifuBackground/content.js
@@ -1,22 +1,37 @@
+var arr = {
+	"root": [{
+			"name": "Asuna Yuuki",
+			"img": "https://user-images.githubusercontent.com/25523604/73164675-62278880-4118-11ea-9954-8d3288b9c6ec.png",
+			"url": "https://myanimelist.net/anime/11757/Sword_Art_Online"
+		}
+	]
+};
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 if(window.location.href == "https://www.google.com/" || window.location.href == "chrome-search://local-ntp/local-ntp.html")
 {
-	document.body.style.backgroundImage = "url(https://user-images.githubusercontent.com/25523604/73164675-62278880-4118-11ea-9954-8d3288b9c6ec.png)";
-}
-
-if(document.getElementsByClassName("FPdoLc tfB0Bf")[0] != undefined)
-{
-	var a = document.createElement('input');
-	a.classList.add('gNO89b');
-	a.setAttribute("value","Go to anime");
-	a.setAttribute("name","btnK");
-	a.setAttribute("data-ved","077ahUKEwj4-rjD_6PnAhVw8XMBHcZGD8sQ4dUDCAo");
-	a.setAttribute("type","submit");
-	document.getElementsByClassName("FPdoLc tfB0Bf")[0].getElementsByTagName('center')[0].appendChild(a);
-}
-if(document.getElementById("SBmmZd") != null )
-{
-	var a = document.createElement('a');
-	a.innerText = "Go to anime";
-	a.setAttribute("href","https://www.google.com");
-	document.getElementById("SBmmZd").appendChild(a);
+	var index = getRandomInt(0,arr["root"].length-1);
+	var waifu = arr["root"][index];
+	document.body.style.backgroundImage = "url("+waifu.img+")";
+	if(document.getElementsByClassName("FPdoLc tfB0Bf")[0] != undefined)
+	{
+		var a = document.createElement('input');
+		a.classList.add('gNO89b');
+		a.setAttribute("value","Go to anime");
+		a.setAttribute("name","btnK");
+		a.setAttribute("data-ved","077ahUKEwj4-rjD_6PnAhVw8XMBHcZGD8sQ4dUDCAo");
+		a.setAttribute("type","submit");
+		a.onclick = function() { window.location = waifu.url; }
+		document.getElementsByClassName("FPdoLc tfB0Bf")[0].getElementsByTagName('center')[0].appendChild(a);
+	}
+	if(document.getElementById("SBmmZd") != null )
+	{
+		var a = document.createElement('a');
+		a.innerText = "Go to anime";
+		a.setAttribute("href", waifu.url);
+		document.getElementById("SBmmZd").appendChild(a);
+	}
 }

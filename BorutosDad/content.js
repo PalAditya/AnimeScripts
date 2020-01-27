03c6cb90fdfd93406dbb0f5962334c862ec95772
PalAditya <adityapal.nghss@gmail.com>
2020-01-27 14:51:04 +0530

    Remove comments, add README

--- BorutosDad/content.js
+++ BorutosDad/content.js
@@ -1,15 +1,3 @@
-/*var text = document.body.innerText;
-var replaced = text.replace(/Naruto/gi,"Boruto's dad");
-//var check = document.createElement('div');
-//div.innerHTML = replaced;
-document.body.innerHTML += replaced;*/
-
-/*var all = document.getElementsByTagName("body *");
-
-for (var i=0, max=all.length; i < max; i++) {
-     // Do something with the element here
-*/
-
 function walkDOM(main) {
     var arr = [];
     var loop = function(main) {
@@ -27,7 +15,6 @@ function walkDOM(main) {
 var arr = walkDOM(document.body);
 for(var i=0; i<arr.length; i++)
 {
-	//console.log(arr[i].innerText);
 	var text = arr[i].innerHTML;
 	var replaced = text.replace(/Naruto/gi,"Boruto's dad");
 	arr[i].innerHTML = replaced;

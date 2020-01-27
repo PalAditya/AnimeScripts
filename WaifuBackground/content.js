954538e0ad7c7d1e0018d510c60080e8052fc9c5
PalAditya <adityapal.nghss@gmail.com>
2020-01-27 20:46:38 +0530

    Add visible button locations

--- WaifuBackground/content.js
+++ WaifuBackground/content.js
@@ -2,3 +2,21 @@ if(window.location.href == "https://www.google.com/" || window.location.href == 
 {
 	document.body.style.backgroundImage = "url(https://user-images.githubusercontent.com/25523604/73164675-62278880-4118-11ea-9954-8d3288b9c6ec.png)";
 }
+
+if(document.getElementsByClassName("FPdoLc tfB0Bf")[0] != undefined)
+{
+	var a = document.createElement('input');
+	a.classList.add('gNO89b');
+	a.setAttribute("value","Go to anime");
+	a.setAttribute("name","btnK");
+	a.setAttribute("data-ved","077ahUKEwj4-rjD_6PnAhVw8XMBHcZGD8sQ4dUDCAo");
+	a.setAttribute("type","submit");
+	document.getElementsByClassName("FPdoLc tfB0Bf")[0].getElementsByTagName('center')[0].appendChild(a);
+}
+if(document.getElementById("SBmmZd") != null )
+{
+	var a = document.createElement('a');
+	a.innerText = "Go to anime";
+	a.setAttribute("href","https://www.google.com");
+	document.getElementById("SBmmZd").appendChild(a);
+}

68aa9a5f58a6a0cde76a46e8577931022491c305
PalAditya <adityapal.nghss@gmail.com>
2020-01-27 16:06:57 +0530

    Add waifu script

---
+++ WaifuBackground/content.js
@@ -0,0 +1,4 @@
+if(window.location.href == "https://www.google.com/" || window.location.href == "chrome-search://local-ntp/local-ntp.html")
+{
+	document.body.style.backgroundImage = "url(https://user-images.githubusercontent.com/25523604/73164675-62278880-4118-11ea-9954-8d3288b9c6ec.png)";
+}

36df583dd4c7799dc2606d2de929f72b9b0dec8b
PalAditya <adityapal.nghss@gmail.com>
2020-01-28 00:47:35 +0530

    Completed ability to save Waifu locally

--- WaifuBackground/content.js
+++ WaifuBackground/content.js
@@ -1,6 +1,6 @@
 var arr = {
 	"root": [{
-			"name": "Asuna Yuuki",
+			"name": "Asuna_Yuuki.png",
 			"img": "https://user-images.githubusercontent.com/25523604/73164675-62278880-4118-11ea-9954-8d3288b9c6ec.png",
 			"url": "https://myanimelist.net/anime/11757/Sword_Art_Online"
 		}
@@ -25,13 +25,29 @@ if(window.location.href == "https://www.google.com/" || window.location.href == 
 		a.setAttribute("data-ved","077ahUKEwj4-rjD_6PnAhVw8XMBHcZGD8sQ4dUDCAo");
 		a.setAttribute("type","submit");
 		a.onclick = function() { window.location = waifu.url; }
-		document.getElementsByClassName("FPdoLc tfB0Bf")[0].getElementsByTagName('center')[0].appendChild(a);
+		var save = document.createElement('input');
+		save.classList.add('gNO89b');
+		save.setAttribute("value","Save waifu");
+		save.setAttribute("name","btnK");
+		save.setAttribute("data-ved","077ahUKEwj4-rjD_6PnAhVw8XMBHcZGD8sQ4dUDCAo");
+		save.setAttribute("type","submit");
+		save.onclick = function() 
+		{ 
+			window.open("https://phptils.herokuapp.com/download.php?url="+waifu.img+"&name="+waifu.name,"_blank");
+		}
+		document.getElementsByClassName("FPdoLc tfB0Bf")[0].getElementsByTagName('center')[0].appendChild(a);document.getElementsByClassName("FPdoLc tfB0Bf")[0].getElementsByTagName('center')[0].appendChild(a);
+		document.getElementsByClassName("FPdoLc tfB0Bf")[0].getElementsByTagName('center')[0].appendChild(a);document.getElementsByClassName("FPdoLc tfB0Bf")[0].getElementsByTagName('center')[0].appendChild(save);
 	}
 	if(document.getElementById("SBmmZd") != null )
 	{
 		var a = document.createElement('a');
 		a.innerText = "Go to anime";
 		a.setAttribute("href", waifu.url);
+		var save = document.createElement('a');
+		save.innerText = "Save waifu";
+		save.setAttribute("href", "https://phptils.herokuapp.com/download.php?url="+waifu.img+"&name="+waifu.name);
+		save.setAttribute("target","_blank");
 		document.getElementById("SBmmZd").appendChild(a);
+		document.getElementById("SBmmZd").appendChild(save);
 	}
 }
